74f4736193f31f5047347e99c14cea3ee73408a1
Scott Sullivan <Scott.Sullivan@argos.co.uk>
2019-07-21 15:58:22 +0100

    updates

--- src/containers/about/About.jsx
+++ src/containers/about/About.jsx
@@ -10,8 +10,8 @@ const About = ({ windowSize, active, 'data-test': dataTest }) => {
         className={`container ${style.about} ${active === 'about' ? style['about-active'] : style['about-inactive']}`}>
         <img
           positionTransition
-          key='profile-image'
-          className={style['profile-pic']}
+          key='bio-image'
+          className={style['bio-pic']}
           width='150'
           height='150'
           initial={{ scale: 1 }}
@@ -19,20 +19,37 @@ const About = ({ windowSize, active, 'data-test': dataTest }) => {
           alt="Scott's profile pic"
           src='https://scontent-lhr3-1.cdninstagram.com/vp/c5cdb50d8b3a1286724a1f27f8c125ab/5DA5F673/t51.2885-15/e35/14134966_1175689132493497_1840210486_n.jpg?_nc_ht=scontent-lhr3-1.cdninstagram.com'
         />
-        <h2>Hi, I’m Scott, nice to meet you.</h2>
+        <h2 className={style['bio-heading']}>Hello, I’m Scott, nice to meet you.</h2>
         <p className={style['bio-text']}>
-          I am a senior ui engineer, specializing in Javascript and front end libraries/frameworks.
-          <ul>
-            <li>- React/Redux & Angular</li>
-            <li>- Web Performance, Bundlesize management and PWA</li>
-            <li>- CMS Headless integration</li>
-            <li>- Global JS libraries and style guide patterns</li>
-            <li>- Node.js, SSR/isomorphic and Express</li>
-            <li>- Jenkins & CI development</li>
-            <li>- Docker Container management</li>
-            <li>- Git standards and Peer Review</li>
-          </ul>
+          I&#39;m a Sr UI Engineer, specializing in Javascript, Node.js, front end libraries and frameworks.
         </p>
+
+        <div className={style['bio-grid']}>
+          <div className={style['bio-grid-item']}>
+            <h3 className={style['bio-grid-title']}>1. Developing</h3>
+            <ul>
+              <li>React · Redux</li>
+              <li>CSS (scss, styled-components, stylus)</li>
+              <li>Web Performance, Webpack & Bundlesize management</li>
+            </ul>
+          </div>
+          <div className={style['bio-grid-item']}>
+            <h3 className={style['bio-grid-title']}>2. Architecture</h3>
+            <ul>
+              <li>Global JS libraries & styleguide patterns</li>
+              <li>CMS Headless integration</li>
+              <li>Git standards and Peer Review</li>
+            </ul>
+          </div>
+          <div className={style['bio-grid-item']}>
+            <h3 className={style['bio-grid-title']}>3. Engineering</h3>
+            <ul>
+              <li>Node.js, Express.js, and SSR</li>
+              <li>Jenkins & CI development</li>
+              <li>Docker Container management</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   )
