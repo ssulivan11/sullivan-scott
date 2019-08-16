9ece521a87fccbfb94e333262e0ff745cbe7d822
Scott Sullivan <Scott.Sullivan@argos.co.uk>
2019-08-17 00:58:43 +0100

    temp

--- cypress/integration/a11y.e2e.js
+++ cypress/integration/a11y.e2e.js
@@ -1,7 +1,13 @@
+const section = {
+  home: '[data-test="home"]',
+  about: '[data-test="about"]',
+  contact: '[data-test="contact"]',
+}
+
 describe('a11y tests', () => {
-  it('should do things', () => {
-    cy.visit('/')
-      .injectAxe()
-      .checkA11y()
+  it('should test standards of contact section', () => {
+    cy.visit('/').get(section.contact)
+    // .injectAxe()
+    // .checkA11y()
   })
 })
