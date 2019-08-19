f45aa2db889c993d84c399dfd38cae0e24dcd1b4
Scott Sullivan <Scott.Sullivan@argos.co.uk>
2019-08-19 10:54:56 +0100

    updates

--- src/containers/about/About.tsx
+++ src/containers/about/About.tsx
@@ -1,6 +1,4 @@
 import * as React from 'react'
-import { motion, AnimatePresence } from 'framer-motion'
-
 import * as style from './about.scss'
 
 interface AboutProps {
@@ -30,63 +28,40 @@ const About: React.FunctionComponent<AboutProps> = ({
       <main
         role='main'
         className={`container ${style.about} ${active === 'about' ? style['about-active'] : style['about-inactive']}`}>
-        <AnimatePresence key={'AnimatePresence-about'}>
-          {active === 'about' && (
-            <React.Fragment>
-              <motion.img
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 1 }}
-                key='bio-image'
-                className={style['bio-image']}
-                width='150'
-                height='150'
-                alt="Scott's profile pic"
-                src={bioImg}
-              />
+        {bioImg && (
+          // @ts-ignore - loading error in ts
+          <img
+            loading='lazy'
+            intrinsicsize='250x200'
+            key='bio-pic'
+            className={style['bio-pic']}
+            width='150'
+            height='150'
+            alt="Scott's profile pic"
+            src={bioImg}
+          />
+        )}
 
-              <motion.h2
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 1 }}
-                key='bio-heading'
-                className={style['bio-heading']}
-                data-test='bio-heading'>
-                {bioHeading}
-              </motion.h2>
-              <motion.p
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 1 }}
-                key='bio-text'
-                className={style['bio-text']}
-                data-test='bio-text'>
-                {bioText}
-              </motion.p>
-
-              <div className={style['bio-grid']}>
-                {skills.map((skill, index) => {
-                  return (
-                    <motion.div
-                      transition={{ delay: (index + 1) / 5 }}
-                      initial={{ y: 10, opacity: 0 }}
-                      animate={{ y: 0, opacity: 1 }}
-                      exit={{ y: 0, opacity: 1 }}
-                      className={style['bio-grid-item']}
-                      key={skill.title}>
-                      <h3 className={style['bio-grid-title']}>{skill.title}</h3>
-                      <ul tabIndex={0}>
-                        {skill.items.map((item) => (
-                          <li key={item}>{item}</li>
-                        ))}
-                      </ul>
-                    </motion.div>
-                  )
-                })}
+        <h2 className={style['bio-heading']} data-test='bio-heading'>
+          {bioHeading}
+        </h2>
+        <p className={style['bio-text']} data-test='bio-text'>
+          {bioText}
+        </p>
+        <div className={style['bio-grid']}>
+          {skills.map((skill) => {
+            return (
+              <div className={style['bio-grid-item']} key={skill.title}>
+                <h3 className={style['bio-grid-title']}>{skill.title}</h3>
+                <ul tabIndex={0}>
+                  {skill.items.map((item) => (
+                    <li key={item}>{item}</li>
+                  ))}
+                </ul>
               </div>
-            </React.Fragment>
-          )}
-        </AnimatePresence>
+            )
+          })}
+        </div>
       </main>
     </div>
   )
