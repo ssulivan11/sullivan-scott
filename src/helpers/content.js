10c8713f0c8b6478f0befa16c4d7a3d60ef2b153
Scott <scott@macbook-air.home>
2019-11-28 15:32:48 +0000

    feat: stylint now not global, react upgrade, typescript upgrade

--- src/helpers/content.js
+++ src/helpers/content.js
@@ -6,7 +6,7 @@ const content = {
   subHeadingText: 'UI Engineer - London / Chicago',
   bioHeading: 'Hello and welcome',
   bioText:
-    "I'm a software engineer with 8+ years experience, who is meticulous about quality, meeting requirements within timelines and pushing technical boundaries to deliver outstanding user experiences.",
+    "I'm an experienced Senior Software Engineer who is meticulous about quality, meeting requirements within timelines and pushing technical boundaries to deliver outstanding user experiences.",
   bioImg:
     'https://scontent-lhr3-1.cdninstagram.com/vp/32a3801cf287c36ce0d8b09cc0551a61/5DE61703/t51.2885-19/51555128_153971348831636_6628374679469948928_n.jpg?_nc_ht=scontent-lhr3-1.cdninstagram.com',
   bioFavorites: '',
@@ -25,11 +25,11 @@ const content = {
     {
       title: 'Architecture',
       items: [
-        'Storybooks Component Libraries',
-        'Style-guide Patterns',
-        'Headless CMS Integration',
-        'Typescript',
         'Boilerplate & Component Scaffolding',
+        'Typescript',
+        'Component based Storybooks',
+        'Headless CMS Integration',
+        'Style-guide Patterns',
         'Git | Gitflow | Peer Review',
       ],
     },
